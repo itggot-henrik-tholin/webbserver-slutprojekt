03724c86df994d4e2dfef44aad9d052df243172d
itggot-henrik-tholin <henrik.tholin@itggot.se>
2018-04-29 21:53:26 +0200

    Added channels

--- app.rb
+++ app.rb
@@ -52,11 +52,33 @@ class App < Sinatra::Base
 		end
 
 		@title = "Yodel"
-		@karma = @user[4]
 
-		@posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+		user_channels = db.execute("SELECT channel FROM channel_membership WHERE account = ?", @user[0])
+		if user_channels.empty?
+			@posts = db.execute("SELECT * FROM posts ORDER BY id DESC")
+		else
+			sql = "SELECT * FROM posts WHERE channel = 0 OR "
+			endsql = " ORDER BY id DESC"
+			user_channels.each_with_index do |usr_chan, i|
+				if i == (user_channels.length - 1)
+					sql += "channel = #{usr_chan.first}"
+				else
+					sql += "channel = #{usr_chan.first} OR "
+				end
+			end
 
+			query = sql + endsql
+			@posts = db.execute(query)
+		end
+
+		@channel_posts = []
 		for post in @posts
+			if post[7] > 0
+				channel = db.execute("SELECT * FROM channels WHERE id = ?", post[7]).first
+				@channel_posts.push(channel)
+			else
+				@channel_posts.push(nil)
+			end
 			comments = db.execute("SELECT COUNT(id) FROM comments WHERE post = ?", post.first).first.first
 			post.push(comments)
 		end
@@ -173,10 +195,14 @@ class App < Sinatra::Base
 
 		text = params['text']
 		coords = params['coords']
+		channel = params['chan']
 
 		if text.empty?
 			flash[:error] = "Yodel cannot be empty."
 			redirect back
+		elsif !channel.empty?
+			db.execute("INSERT INTO posts (text, owner, coords, color, channel) VALUES (?, ?, ?, ?, ?)", text, @user[0], coords, colors.sample, channel)
+			redirect "/channel/#{channel}"
 		else
 			db.execute("INSERT INTO posts (text, owner, coords, color) VALUES (?, ?, ?, ?)", text, @user[0], coords, colors.sample)
 			redirect '/'
@@ -203,7 +229,7 @@ class App < Sinatra::Base
 		id = params['id']
 
 		voteCheck = db.execute("SELECT * FROM votes WHERE post = ? AND account = ?", id, @user[0]).first
-		unless voteCheck.empty?
+		unless voteCheck.nil? || voteCheck.empty?
 			flash[:error] = "You have already voted"
 			redirect back
 		end
@@ -227,7 +253,7 @@ class App < Sinatra::Base
 		id = params['id']
 
 		voteCheck = db.execute("SELECT * FROM votes WHERE post = ? AND account = ?", id, @user[0])
-		unless voteCheck.empty?
+		unless voteCheck.nil? || voteCheck.empty?
 			flash[:error] = "You have already voted"
 			redirect back
 		end
@@ -319,5 +345,105 @@ class App < Sinatra::Base
 			# -2 karma for each upvote
 			db.execute("UPDATE accounts SET karma = karma - 2 WHERE username = ?", @user[1])
 		end
+	end
+
+	get '/channels' do
+		unless @user
+			redirect '/login'
+		end
+
+		@channels = db.execute("SELECT * FROM channels")
+		for channel in @channels
+			member_count = db.execute("SELECT COUNT(id) FROM channel_membership WHERE channel = ?", channel[0]).first.first
+			channel.push(member_count)
+		end
+
+		channel_membership = db.execute("SELECT * FROM channel_membership WHERE account = ?", @user[0])
+		@user_channels = []
+		for channel in channel_membership
+			member = db.execute("SELECT * FROM channels WHERE id = ?", channel[1]).first
+			member_count = db.execute("SELECT COUNT(id) FROM channel_membership WHERE channel = ?", channel[1]).first.first
+			member.push(member_count)
+			@user_channels.push(member)
+
+			@channels.each_with_index do |c, i|
+				if c[0] == channel[1]
+					@channels.delete_at(i)
+				end
+			end
+		end
+
+		slim :channels
+	end
+
+	get '/channel/new' do
+		unless @user
+			redirect '/login'
+		end
+
+		slim :new_channel
+	end
+
+	post '/channel/new' do
+		unless @user
+			redirect '/login'
+		end
+
+		name = params['name']
+		unless name.nil?
+			db.execute("INSERT INTO channels (name) VALUES (?)", name)
+		end
+		redirect '/channels'
+	end
+
+	get '/channel/:id' do
+		unless @user
+			redirect '/login'
+		end
+
+		id = params['id'].to_i
+		if id <= 0
+			redirect '/'
+		end
+
+		@channel = db.execute("SELECT * FROM channels WHERE id = ?", id).first
+		memberCheck = db.execute("SELECT id FROM channel_membership WHERE channel = ? AND account = ?", id, @user[0]).first
+		@member = memberCheck.nil? ? false : true
+
+		@posts = db.execute("SELECT * FROM posts WHERE channel = ? ORDER BY id DESC", id)
+		for post in @posts
+			comments = db.execute("SELECT COUNT(id) FROM comments WHERE post = ?", post.first).first.first
+			post.push(comments)
+		end
+
+		slim :channel
+	end
+
+	get '/channel/:id/join' do
+		unless @user
+			redirect '/login'
+		end
+
+		id = params['id']
+		channelCheck = db.execute("SELECT id FROM channels WHERE id = ?", id).first
+		unless channelCheck.nil?
+			db.execute("INSERT INTO channel_membership (channel, account) VALUES (?, ?)", id, @user[0])
+		end
+
+		redirect back
+	end
+
+	get '/channel/:id/leave' do
+		unless @user
+			redirect '/login'
+		end
+
+		id = params['id']
+		channelCheck = db.execute("SELECT id FROM channels WHERE id = ?", id).first
+		unless channelCheck.nil?
+			db.execute("DELETE FROM channel_membership WHERE channel = ? AND account = ?", id, @user[0])
+		end
+
+		redirect back
 	end
 end
